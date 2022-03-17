0a813251e388c02c81777e20ffe479a62b87910b
unknown <minhnguyenquocnhat@gmail.com>
2022-03-18 01:26:59 +0700

    'l'

--- js/script.js
+++ js/script.js
@@ -1,16 +1,59 @@
 // console.log(document.getElementById("title"));
 
 // console.log(document instanceof HTMLDocument);
-function sayHello() {
-    var name =document.getElementById("name").value;
-    var message = "<h2>Hello " + name + "!</h2>";
-    
-    //document.getElementById("content").textContent = message;
-    document.getElementById("content").innerHTML = message;
+document.addEventListener("DOMContentLoaded",
+    function (event) {
 
-    if(name==="student") {
-        var title = document.querySelector("#title").textContent;
-        title += "& lovin' it";
-        document.querySelector("h1").textContent = title;
+        function sayHello(event) {
+            console.log(event);
+            this.textContent = " said it ";
+            var name = document.getElementById("name").value;
+            var message = "<h2>Hello " + name + "!</h2>";
+            
+            //document.getElementById("content").textContent = message;
+            document.getElementById("content").innerHTML = message;
+        
+            if(name==="student") {
+                var title = document.querySelector("#title").textContent;
+                title += "& lovin' it";
+                document.querySelector("h1").textContent = title;
+            }
+        };
+        
+        document.querySelector("button")
+            .addEventListener("click", sayHello);
+
+        document.querySelector("body")
+            .addEventListener("mousemove", 
+            function(event){
+                
+                if(event.shiftKey === true){
+                console.log("x: "+event.clientX);
+                console.log("y: "+event.clientY);
+                }
+            });
     }
-}
+);
+
+
+// function sayHello() {
+//     this.textContent = " said it ";
+//     var name = document.getElementById("name").value;
+//     var message = "<h2>Hello " + name + "!</h2>";
+    
+//     //document.getElementById("content").textContent = message;
+//     document.getElementById("content").innerHTML = message;
+
+//     if(name==="student") {
+//         var title = document.querySelector("#title").textContent;
+//         title += "& lovin' it";
+//         document.querySelector("h1").textContent = title;
+//     }
+// };
+
+//     document.querySelector("button")
+//      .addEventListener("click", sayHello);
+
+
+// document.querySelector("button")
+//     .onclick = sayHello;
